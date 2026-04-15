7ff24132b49da7096e48f179863ade9d21cde10f
lukasarteni <lukasarteni@gmail.com>
2026-04-15 03:26:18 -0400

    added some new code for making visuals

--- Assets/LukasMadeStuff/scripts/InTempoScripts/HitManager.cs
+++ Assets/LukasMadeStuff/scripts/InTempoScripts/HitManager.cs
@@ -8,20 +8,48 @@ public class HitManager : MonoBehaviour
 
     public NoteSpawner spawner;
 
-    public float perfectWindow = 0.08f;
-    public float goodWindow = 0.15f;
+    public float perfectWindow = 0.1f;
+    public float goodWindow = 0.5f;
 
     void Update()
     {
-        if (Keyboard.current.gKey.wasPressedThisFrame)
-            TryHit("G");
+        if (Keyboard.current.aKey.wasPressedThisFrame & Keyboard.current.zKey.wasPressedThisFrame)
+            Debug.Log("Am2");
+        if (Keyboard.current.shiftKey.wasPressedThisFrame)
+        {
+            if (Keyboard.current.aKey.wasPressedThisFrame){
+                TryHit("Am");
+                Debug.Log("Am");}
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+                TryHit("Em");
+            if (Keyboard.current.dKey.wasPressedThisFrame)
+                TryHit("Dm");
+        }
+        else
+        {
+            if (Keyboard.current.aKey.wasPressedThisFrame)
+                TryHit("A");
+            if (Keyboard.current.bKey.wasPressedThisFrame){
+                TryHit("B");
+                 Debug.Log("this is where B was pressed");}
+            if (Keyboard.current.cKey.wasPressedThisFrame)
+                TryHit("C");
 
-        if (Keyboard.current.eKey.wasPressedThisFrame)
-            TryHit("Em");
+            if (Keyboard.current.dKey.wasPressedThisFrame)
+                TryHit("D");
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+                TryHit("E");
+            if (Keyboard.current.fKey.wasPressedThisFrame)
+                TryHit("F");
+
+            if (Keyboard.current.gKey.wasPressedThisFrame)
+                TryHit("G");
+        }
     }
 
-    void TryHit(string chord)
+    void TryHit(string chordSent)
     {
+        
         float songTime = music.time;
 
         NoteData closest = null;
@@ -29,8 +57,10 @@ public class HitManager : MonoBehaviour
 
         foreach (var note in spawner.notes)
         {
-            if (note.hit) continue;
-            if (note.chord != chord) continue;
+            if (note.hit)
+                continue;
+            if (note.chord != chordSent)
+                continue;
 
             float error = Mathf.Abs(songTime - note.time);
 
@@ -41,7 +71,8 @@ public class HitManager : MonoBehaviour
             }
         }
 
-        if (closest == null) return;
+        if (closest == null)
+            return;
 
         if (closestError <= goodWindow)
         {
@@ -50,7 +81,7 @@ public class HitManager : MonoBehaviour
             if (closest.obj != null)
                 Destroy(closest.obj);
 
-            Debug.Log("HIT " + chord);
+            Debug.Log("HIT " + chordSent);
         }
         else
         {
--- Assets/LukasMadeStuff/scripts/InTempoScripts/LaneManager.cs
+++ Assets/LukasMadeStuff/scripts/InTempoScripts/LaneManager.cs
@@ -5,9 +5,16 @@ public class LaneManager : MonoBehaviour
 {
     public Transform scoringPlank;
 
+    public GameObject aTargetPrefab;
+    public GameObject bTargetPrefab;
+    public GameObject cTargetPrefab;
+    public GameObject dTargetPrefab;
+    public GameObject eTargetPrefab;
+    public GameObject fTargetPrefab;
     public GameObject gTargetPrefab;
+    public GameObject amTargetPrefab;
+    public GameObject dmTargetPrefab;
     public GameObject emTargetPrefab;
-
     public float scrollSpeed = 5f;
 
     const float fixedY = 1.85f;
@@ -19,24 +26,30 @@ public class LaneManager : MonoBehaviour
     {
         prefabMap = new Dictionary<string, GameObject>()
         {
+            { "A", aTargetPrefab },
+            { "B", bTargetPrefab },
+            { "C", cTargetPrefab },
+            { "D", dTargetPrefab },
+            { "E", eTargetPrefab },
+            { "F", fTargetPrefab },
             { "G", gTargetPrefab },
-            { "Em", emTargetPrefab }
+            { "Am", amTargetPrefab },
+            { "Dm", dmTargetPrefab },
+            { "Em", emTargetPrefab },
         };
     }
 
     public GameObject SpawnNote(NoteData note, float songTime)
     {
         GameObject prefab = prefabMap[note.chord];
+        if (prefab == null)
+            return null;
 
         float timeUntilHit = note.time - songTime;
 
         float xOffset = timeUntilHit * scrollSpeed;
 
-        Vector3 spawnPos = new Vector3(
-            scoringPlank.position.x + xOffset,
-            fixedY,
-            fixedZ
-        );
+        Vector3 spawnPos = new Vector3(scoringPlank.position.x + xOffset, fixedY, fixedZ);
 
         GameObject obj = Instantiate(prefab, spawnPos, Quaternion.identity);
 
